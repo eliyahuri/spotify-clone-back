d3e902778a176aee477a688bb2d8018ce14dfbf8
eliyahuri <tkhvu1236@gmail.com>
2024-08-11 00:41:22 +0300

    chore: Update router configuration to use nested routes for user endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import router from "./routers";
+import router from "./routers/index.router";
 import config from "./config/config";
 import cors, { type CorsOptions } from "cors";
 
@@ -9,7 +9,7 @@ const corsOptions: CorsOptions = config.cors;
 app.use(express.json());
 app.use(cors(corsOptions));
 
-app.use("/", router);
+app.use("/api", router);
 
 app.listen(config.port, () => {
   console.log(`Server is running on http://localhost:${config.port}`);
