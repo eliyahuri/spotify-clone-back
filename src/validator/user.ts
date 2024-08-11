8097a707f1328c25d977c65fb5058b759611c074
eliyahuri <tkhvu1236@gmail.com>
2024-08-11 17:28:24 +0300

    chore: Update user validation schema to include updateUser fields

--- src/validator/user.ts
+++ src/validator/user.ts
@@ -20,3 +20,61 @@ export const createUserSchema = z.object({
     }),
   }),
 });
+
+export const updateUserSchema = z.object({
+  name: z.string().min(3).optional(),
+  email: z.string().email().optional(),
+  password: z.string().min(6).optional(),
+  image: z
+    .object({
+      connect: z
+        .object({
+          id: z.string(),
+        })
+        .optional(),
+      create: z
+        .object({
+          url: z.string().url(),
+        })
+        .optional(),
+      connectOrCreate: z
+        .object({
+          where: z.object({
+            id: z.string(),
+          }),
+          create: z.object({
+            url: z.string().url().optional(),
+          }),
+        })
+        .optional(),
+    })
+    .optional(),
+  playlists: z
+    .array(
+      z.object({
+        connect: z.object({
+          id: z.string(),
+        }),
+        create: z.object({
+          name: z.string(),
+          songs: z.array(
+            z.object({
+              connect: z.object({
+                id: z.string(),
+              }),
+              connectOrCreate: z.object({
+                where: z.object({
+                  id: z.string(),
+                }),
+                create: z.object({
+                  name: z.string(),
+                  url: z.string().url(),
+                }),
+              }),
+            })
+          ),
+        }),
+      })
+    )
+    .optional(),
+});
